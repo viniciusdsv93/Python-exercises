7aaf9a82e3333ad8adcdd32b5af1703ccf27f8b2
viniciusdsv93 <86579020+viniciusdsv93@users.noreply.github.com>
2021-07-09 22:19:04 -0300

    Add files via upload

---
+++ 2_age.py
@@ -0,0 +1,7 @@
+#Program that asks the user's birthday and print a message.
+
+day = input("Day: ")
+month = input("Month: ").title()
+year = input("Year: ")
+
+print ("Is your birthday on {} of {} of {}?".format(day,month,year))
