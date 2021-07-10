1f044150bcbabf9bb53d81f98431d42a11e1032f
viniciusdsv93 <86579020+viniciusdsv93@users.noreply.github.com>
2021-07-09 22:19:17 -0300

    Add files via upload

---
+++ 3_sum.py
@@ -0,0 +1,6 @@
+#Program that calculates the sum between two numbers digited by the user.
+
+n1 = int(input('First number: '))
+n2 = int(input('Second number: '))
+
+print("The sum between {} and {} equals {}".format(n1,n2,n1+n2))
