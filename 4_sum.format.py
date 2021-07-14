f5fe1dca011363713f8f897adf9d6dcc77612355
viniciusdsv93 <86579020+viniciusdsv93@users.noreply.github.com>
2021-07-14 08:33:06 -0300

    Add files via upload

---
+++ 4_sum.format.py
@@ -0,0 +1,6 @@
+#Program that calculates the sum between two numbers digited by the user.
+
+number1 = int(input("First number: "))
+number2 = int(input("Second number: "))
+sum = number1 + number2
+print("The result is {}".format(sum)) 
