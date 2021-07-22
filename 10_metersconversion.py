b111fe963398a747524eef4443097f16b67b7c34
viniciusdsv93 <86579020+viniciusdsv93@users.noreply.github.com>
2021-07-21 21:36:09 -0300

    Add files via upload

---
+++ 10_metersconversion.py
@@ -0,0 +1,5 @@
+# Program that converts the distance in meters entered by the user to centimeters and milimeters.
+
+number = int(input('\nEnter the distance in meters: '))
+
+print(f'\nThe distance of {number} meters is equal to {number * 100} centimeters or {number * 1000} milimeters.\n')
