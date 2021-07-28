024e3bce1f31fe32e9b50b7cf08500cfb0fa6b0d
viniciusdsv93 <86579020+viniciusdsv93@users.noreply.github.com>
2021-07-28 07:46:54 -0300

    Add files via upload

---
+++ 14_priceDiscount.py
@@ -0,0 +1,4 @@
+# Program that calculates the price of a product with 5% of discount.
+
+price = float(input('Enter the price of the product: '))
+print(f'The price of the product with 5% of discount is {price * 0.95} dolars')
