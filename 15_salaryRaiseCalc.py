6150a0c8702da3c3afb20be99850fc0d145e4f2a
viniciusdsv93 <86579020+viniciusdsv93@users.noreply.github.com>
2021-07-30 07:58:40 -0300

    Add files via upload

---
+++ 15_salaryRaiseCalc.py
@@ -0,0 +1,5 @@
+# Program that calculates a worker's salary raise.
+
+newSalary = float(input('Enter the worker\'s salary: '))
+
+print(f'The salary with a raise of 15% is equals to US$ {newSalary * 1.15}.')
