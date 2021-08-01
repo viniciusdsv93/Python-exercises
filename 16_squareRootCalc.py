6ee595d4e49dea2fdb1b65d01f8a9abfe5cba7f6
viniciusdsv93 <86579020+viniciusdsv93@users.noreply.github.com>
2021-07-31 22:55:23 -0300

    Add files via upload

---
+++ 16_squareRootCalc.py
@@ -0,0 +1,8 @@
+# Program that calculates a number's square root.
+
+import math
+
+number = int(input('Enter a number: '))
+squareRoot = math.sqrt(number)
+
+print(f'The square root of {number} is {squareRoot}')
