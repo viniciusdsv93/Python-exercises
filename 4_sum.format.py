206301fbd03e8fce6053c79739c2e9b861d67cc6
viniciusdsv93 <86579020+viniciusdsv93@users.noreply.github.com>
2021-08-02 22:26:49 -0300

    Update and rename 4_sum.format.py to 04_sum.format.py

--- 4_sum.format.py
+++
@@ -1,6 +0,0 @@
-#Program that calculates the sum between two numbers digited by the user.
-
-number1 = int(input("First number: "))
-number2 = int(input("Second number: "))
-sum = number1 + number2
-print("The result is {}".format(sum)) 
