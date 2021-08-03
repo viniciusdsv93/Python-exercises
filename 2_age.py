130bed7f3a7e0ae2c59f20bc999c005d72ceb955
viniciusdsv93 <86579020+viniciusdsv93@users.noreply.github.com>
2021-08-02 22:21:21 -0300

    Update and rename 2_age.py to 02_age.py

--- 2_age.py
+++
@@ -1,7 +0,0 @@
-#Program that asks the user's birthday and print a message.
-
-day = input("Day: ")
-month = input("Month: ").title()
-year = input("Year: ")
-
-print ("Is your birthday on {} of {} of {}?".format(day,month,year))
