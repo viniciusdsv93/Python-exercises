a5d04dbf525f6ebacf79a1b1841abd168a7313d3
viniciusdsv93 <86579020+viniciusdsv93@users.noreply.github.com>
2021-08-02 22:28:02 -0300

    Update and rename 7_sucessorpredecessor.py to 07_sucessorpredecessor.py

---
+++ 07_sucessorpredecessor.py
@@ -0,0 +1,7 @@
+# Program that prints the sucessor and the predecessor of the number entered by the user
+
+number = int(input('Enter a number: '))
+predecessor = number - 1
+sucessor = number + 1
+
+print('About the number {}, the predecessor is  {} and the sucessor is {}'.format(number, predecessor, sucessor))
--- 7_sucessorpredecessor.py
+++
@@ -1,7 +0,0 @@
-#Program that prints the sucessor and the predecessor of the number digited by the user.
-
-number = int(input("Digit one number: "))
-predecessor = number - 1
-sucessor = number + 1
-
-print("About the number {}, the predecessor is  {} and the sucessor is {}".format(number,predecessor,sucessor))
