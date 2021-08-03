0c5e57020b7992f0785df101d48ef7b97f56cdda
viniciusdsv93 <86579020+viniciusdsv93@users.noreply.github.com>
2021-08-02 22:28:43 -0300

    Update and rename 9_averagegrade.py to 09_averagegrade.py

--- 9_averagegrade.py
+++
@@ -1,6 +0,0 @@
-# Program that calculates de average grade of a student.
-
-number1 = int(input("Digit the first grade of the student: "))
-number2 = int(input("Digit the second grade of the student: "))
-
-print("The student's average grade is {}".format((number1 + number2) / 2))
