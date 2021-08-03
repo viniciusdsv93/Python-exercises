75c38e43493840c6c75a3c970306aa03a24e1db4
viniciusdsv93 <86579020+viniciusdsv93@users.noreply.github.com>
2021-08-02 22:26:07 -0300

    Update and rename 3_sum.py to 03_sum.py

---
+++ 03_sum.py
@@ -0,0 +1,6 @@
+# Program that calculates the sum between two numbers entered by the user
+
+num1 = int(input('First number: '))
+num2 = int(input('Second number: '))
+
+print('The sum between {} and {} is {}'.format(num1, num2, num1 + num2))
