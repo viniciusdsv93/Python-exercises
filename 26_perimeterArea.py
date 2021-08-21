41ccddbc5e2436e62f733ab4242b63b78ea4c359
viniciusdsv93 <86579020+viniciusdsv93@users.noreply.github.com>
2021-08-21 10:43:38 -0300

    Add files via upload

---
+++ 26_perimeterArea.py
@@ -0,0 +1,6 @@
+# Program that calculates the perimeter and the area of area square
+
+size = int(input('Enter the size of the square in centimeters: '))
+perimeter = size * 4
+area = size ** 2
+print('perimeter:',perimeter,'- area:',area)
